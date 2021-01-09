ee93128c27aeceb5d755edd3d18577cb4aa5f0b7
Adwithya <avmagow@gmail.com>
2021-01-09 19:34:37 +0800

    numpy approach to calculate quantiles

--- Data Exploration/calculate_quantiles.py
+++ Data Exploration/calculate_quantiles.py
@@ -1,31 +1,44 @@
 import json
-import numpy
+import numpy as np
 import pandas as pd
 import os
 
 # URI for the files
 relative_uri_SICP = os.path.join("..", "SICP", "daily")
 relative_uri_json = ""
-quantile_per_track = {}
+quantile_per_station_track = {}
 quantile = 0.90
 output_file = "act_occupied_time_quantile.json"
 quantile_column = "act_occupied_time"
 
+
+def fn_get_group_by_list(dataframe):
+    key_station, key_track, values = (
+        dataframe.sort_values(["station", "track"])
+        .filter(["station", "track", quantile_column])
+        .values.T
+    )
+    keys = [key_station[i] + "-" + key_track[i] for i in range(len(key_station))]
+    ukeys, index = np.unique(keys, True)
+    arrays = np.split(values, index[1:])
+    return {"station-track": ukeys, "act_occupied_time": [list(a) for a in arrays]}
+
+
 for dir_name in os.listdir(relative_uri_SICP):
     relative_uri_csv = relative_uri_SICP + "/" + dir_name
     dataframe = pd.read_csv(relative_uri_csv)
-    for index, _ in dataframe.iterrows():
-        track_no = dataframe.loc[[index], "track"].values[0]
-        station = dataframe.loc[[index], "station"].values[0]
-        track_station_pair = str(track_no) + "-" + str(station)
-        if track_station_pair not in quantile_per_track:
-            quantile_per_track[track_station_pair] = []
-        quantile_per_track[track_station_pair].append(
-            dataframe.loc[[index], quantile_column].values[0]
-        )
+    return_dict = fn_get_group_by_list(dataframe)
+    for i in range(len(return_dict["station-track"])):
+        if return_dict["station-track"][i] not in quantile_per_station_track:
+            quantile_per_station_track[return_dict["station-track"][i]] = []
+        quantile_per_station_track[return_dict["station-track"][i]] += return_dict[
+            quantile_column
+        ][i]
 
-for key in quantile_per_track:
-    quantile_per_track[key] = numpy.array(quantile_per_track[key]).quantile(quantile)
+for key in quantile_per_station_track:
+    quantile_per_station_track[key] = np.nanquantile(
+        quantile_per_station_track[key], quantile
+    )
 
 with open(output_file, "w") as file:
-    json.dump(quantile_per_track, file)
+    json.dump(quantile_per_station_track, file)
