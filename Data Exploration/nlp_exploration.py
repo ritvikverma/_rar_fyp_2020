b1fa22b361d77389fe9efed33ef3a99131723147
Ritvik Verma <ritvik.verma@chekk.me>
2021-02-02 22:59:30 +0800

    text file processing

--- Data Exploration/nlp_exploration.py
+++ Data Exploration/nlp_exploration.py
@@ -1,5 +1,6 @@
 import json
 import os
+from collections import Counter
 
 relative_uri_accidents_records = os.path.join("../accidents_record", "logs")
 accident_jsons = {}
@@ -12,8 +13,22 @@ def populate_accidents_dict():
             accident_jsons[accident_json_name.split('.')[0].split()[1]] = retrieved_json['events']
 
 
+def get_repeated_data(text_descriptions):
+    returnable = [k for k, v in Counter(text_descriptions).items() if v > 1]
+    returnable = [description.lower() for description in returnable]
+    return list(set(returnable))
+
+
 if __name__ == '__main__':
     populate_accidents_dict()
+    text_descriptions = []
     for day, events in accident_jsons.items():
         for event in events:
-            print(event['Fault Description'])
+            has_train_in_any_event = False
+            for desc in event['event_descriptions']:
+                if desc['Train No'].strip() != "":
+                    has_train_in_any_event = True
+            if has_train_in_any_event:
+                text_descriptions.append(event["Fault Description"])
+    print(len(text_descriptions))
+    print(get_repeated_data(text_descriptions))
