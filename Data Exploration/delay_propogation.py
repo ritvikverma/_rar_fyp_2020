281c7be0f02901e5a0601a5756045127f4837462
Adwithya <avmagow@gmail.com>
2021-03-04 14:15:43 +0800

    Fix initial delay prop algo

--- Data Exploration/delay_propogation.py
+++ Data Exploration/delay_propogation.py
@@ -9,11 +9,12 @@ def initialize_variables():
     config["relative_uri_SICP"] = os.path.join("..", "SICP", "incident")
 
     # Algo dependent variables
-    config["time_range_min"] = 60
+    config["quantile_column_being_checked"] = "act_travelling_time"
+    config["time_range_min"] = 5
 
     # Column being added to the CSV files
-    config["columns_added"] = ("propagated_incident", )
-    config["columns_added_default_value"] = (False, )
+    config["columns_added"] = ("propagated_incident", "propagated_fault")
+    config["columns_added_default_value"] = (False, "")
 
     # For checking with all quantiles
     config["all_quantiles"] = (95, 90, 85, 80, 75, 70)
@@ -50,17 +51,15 @@ def filter_dataframe(config, dataframe, incident):
             dataframe, incident, "act_arr_time"), "act_arr_time", config["time_range_min"]
     )
     train_number_mask = dataframe[config["physical_train_number_column"]
-                                  ] == get_value_from_incident(
+                                  ] != get_value_from_incident(
         dataframe, incident, config["physical_train_number_column"])
 
-    query = dataframe.index[station_mask &
-                            track_mask & datetime_mask & train_number_mask].sort_values(["act_arr_time"])
-    print(query)
-
+    query = dataframe.index[station_mask & track_mask & datetime_mask & train_number_mask].sort_values(
+        ["act_arr_time"])
     return query
 
 
-def check_delay_propogation(config, dataframe):
+def check_delay_propogation(config, dataframe, relative_uri_csv):
     incidents = dataframe[dataframe["incident"] == True]
 
     for incident in incidents.itertuples():
@@ -68,11 +67,14 @@ def check_delay_propogation(config, dataframe):
         for index in query[0]:
             is_incident, _ = check_quantile_track(
                 config["all_quantiles"], config["list_of_quant_dicts"], config["quantile_column_being_checked"], index, dataframe)
-            added_tuple = (is_incident, )
+            added_tuple = (is_incident, get_value_from_incident(
+                dataframe, incident, "fault_description"))
             if is_incident:
                 for i in range(len(config["columns_added"])):
                     dataframe.at[index,
                                  config["columns_added"][i]] = added_tuple[i]
+
+    dataframe.to_csv(relative_uri_csv, index=False)
 
 
 if __name__ == '__main__':
@@ -80,4 +82,4 @@ if __name__ == '__main__':
     for dir_name in os.listdir(config["relative_uri_SICP"]):
         relative_uri_csv = os.path.join(config["relative_uri_SICP"], dir_name)
         dataframe = read_csv(config, relative_uri_csv)
-        check_delay_propogation(config, dataframe)
+        check_delay_propogation(config, dataframe, relative_uri_csv)
