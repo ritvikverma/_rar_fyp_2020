bb94e882075f6b41f742f9f64b2da1488456073b
rhea1310 <guptarhea13@gmail.com>
2021-03-25 12:58:26 +0800

    csv files added with data analysis

--- Data Exploration/graphing.py
+++ Data Exploration/graphing.py
@@ -26,20 +26,30 @@ station_list = [
 df = pd.read_csv('../SICP/lstm_only_test_result_mar11.csv')
 sns.set(style="darkgrid")
 
-df = df[df["current_time"].str.contains("2019-11-03")]
+# df = df[df["current_time"].str.contains("2019-11-03")]
+cols = [2, 20, 21, 22]
+df = df[df.columns[cols]]
+df = df[df["day_in_week"] == 1]
 df = df[df["lstm_mae"] >= 60]
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
 
 x = np.array([station_list.index(element) for element in df['Sn']])
 y = df['hour_in_day']
 z = df['lstm_mae']
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
 ax.set_xlabel("station")
 ax.set_ylabel("hour in day")
 ax.set_zlabel("lstm_mae")
 
-ax.scatter(x, y, z)
+ax.scatter(x, y, z, c=z)
 
+plt.show()
+plt.savefig()
+
+plt.scatter(x, y, c=z, s=5)
+cbar = plt.colorbar()
+plt.xlabel("station")
+plt.ylabel("hour in day")
 plt.show()
